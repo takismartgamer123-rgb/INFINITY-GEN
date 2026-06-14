060e20eb8af1f75e3e998adb85e1bb108a9c1858
takismartgamer123-rgb <takismartgamer123@gmail.com>
2026-06-14 21:03:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import threading, os
-from flask import Flask
-from chat_handler import start_chat_loop
-from stream import run_stream_thread
+import os, json
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "✅ INFINITY GEN خدام من قالمة 🚫💸 | 16 لعبة + متجر + مافيا + بث 24/7"
-
-def run_bot():
-    print("🔥 نشغلو لوب الشات...")
-    start_chat_loop()
-
-# نشغلو البث 24/7
-run_stream_thread()
-
-# نشغلو البوت
-threading.Thread(target=run_bot, daemon=True).start()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+def get_youtube():
+    TOKEN_JSON = os.environ.get("TOKEN_JSON")
+    creds = Credentials.from_authorized_user_info(json.loads(TOKEN_JSON))
+    if creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    return build("youtube", "v3", credentials=creds)
